4310fb4053048b80ae0e770acea21f0404c79654
LinconDC <linconmartins18@gmail.com>
2022-05-23 16:29:24 -0300

    New class Conta

--- java-pilha/src/Fluxo.java
+++ java-pilha/src/Fluxo.java
@@ -3,16 +3,19 @@ public class Fluxo {
 	public static void main(String[] args) {
 		System.out.println("Ini do main");
 		try {
-		metodo1();
-		System.out.println("Fim do main");
+			metodo1();
 		} catch (ArithmeticException ex) {
-			System.out.println("ArithmeticException");
+//			String msg = ex.getMessage();
+//			System.out.println("ArithmeticException" + msg);
+			ex.printStackTrace();
 		}
+		System.out.println("Fim do main");
+
 	}
 
 	private static void metodo1() {
 		System.out.println("Ini do metodo1");
-			metodo2();
+		metodo2();
 		System.out.println("Fim do metodo1");
 	}
 
